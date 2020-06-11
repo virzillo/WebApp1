34c5a0d632a1e9135c63fa7b3b9f14ad4288331c
Riccardo Virgilio <riccardo.virgilio@gmail.com>
2020-06-11 20:45:06 +0200

    alert toastr ok

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -54,7 +54,28 @@
 
         {{-- Includable JS --}}
         @yield('scripts')
+        <script>
+            @if(Session::has('message'))
+            var type = "{{ Session::get('alert-type') }}";
+            switch(type){
+                case 'info':
+                    toastr.info("{{ Session::get('message') }}");
+                    break;
 
+                case 'warning':
+                    toastr.warning("{{ Session::get('message') }}");
+                    break;
+
+                case 'success':
+                    toastr.success("{{ Session::get('message') }}");
+                    break;
+
+                case 'error':
+                    toastr.error("{{ Session::get('message') }}");
+                    break;
+            }
+            @endif
+        </script>
     </body>
 </html>
 
