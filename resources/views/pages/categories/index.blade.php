326f1cfdcd065459242e83e0651772f673fa3fb7
Riccardo Virgilio <riccardo.virgilio@gmail.com>
2020-06-25 19:48:44 +0200

    creato script per datatable

--- resources/views/pages/categories/index.blade.php
+++ resources/views/pages/categories/index.blade.php
@@ -9,7 +9,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Crea Category</h5>
+                <h5 class="modal-title">Crea Categoria</h5>
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
@@ -84,16 +84,7 @@
                         data-target="#createCategoryModal">
                         <span class="svg-icon svg-icon-md">
                             <!--begin::Svg Icon | path:assets/media/svg/icons/Design/Flatten.svg-->
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <rect x="0" y="0" width="24" height="24" />
-                                    <circle fill="#000000" cx="9" cy="15" r="6" />
-                                    <path
-                                        d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z"
-                                        fill="#000000" opacity="0.3" />
-                                </g>
-                            </svg>
+                           {{Metronic::getSVG("assets/media/svg/icons/Design/Flatten.svg")}}
                             <!--end::Svg Icon-->
                         </span>Crea nuovo</a>
                     <!--end::Button-->
@@ -108,7 +99,6 @@
                 <table class="table table-bordered table-vertical-center" id="kt_datatable">
                     <thead>
                         <tr>
-                            <th></th>
                             <th>ID</th>
                             <th>Titolo</th>
                             <th>Pubbicato</th>
@@ -118,7 +108,6 @@
                     <tbody>
                         @foreach($categories as $category)
                             <tr>
-                                <td></td>
                                 <td>{{ $category->id }}</td>
                                 <td>{{ $category->titolo }}</td>
                                 <td>
@@ -238,13 +227,17 @@
     type="text/javascript"></script>
 
 {{-- page scripts --}}
-<script src="{{ asset('js/pages/crud/datatables/basic/basic.js') }}" type="text/javascript">
-</script>
+{{-- datatable init script --}}
+<script src="{{ asset('js/pages/my-script.js') }}" type="text/javascript"></script>
 
 <script src="{{ asset('js/pages/features/miscellaneous/sweetalert2.js') }}"
     type="text/javascript"></script>
 <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 <script>
+
+
+
+
     function ConfirmDelete() {
 
         var x = confirm("Are you sure you want to delete?");
